4a2c5e7a808bb0f4543c2608aaff2fe8333f7f12
Sathyanarayanan Gunasekaran <gsathya.ceg@gmail.com>
2014-06-09 14:31:25 -0700

    port to emscripten

--- pong.c
+++ pong.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
-#include <sdl2/SDL.h>
-#include <sdl2/SDL_image.h>
-#include "pong.h"
+#include <SDL.h>
+#include <SDL_image.h>
+
+#include <emscripten/emscripten.h>
 
 static const int SCREEN_WIDTH = 640;
 static const int SCREEN_HEIGHT = 480;
@@ -9,8 +10,24 @@ static const char *title_img_path = "x.png";
 static const char *ball_img_path = "dot.bmp";
 static const int velocity = 1;
 
+#define TRUE 1
+#define FALSE 0
+
 int ball_w;
 int ball_h;
+
+static int quit = FALSE;
+static int show_title = TRUE;
+
+static SDL_Window *window = NULL;
+static SDL_Surface *screen_surface = NULL;
+static SDL_Surface *png_surface = NULL;
+static SDL_Texture *texture = NULL;
+static SDL_Texture *ball_texture = NULL;
+static SDL_Renderer *renderer = NULL;
+
+SDL_Rect player1, player2;
+int player1_vel = 0, player2_vel = 0;
 
 int init(SDL_Window **window, SDL_Renderer **renderer) {
   if( SDL_Init( SDL_INIT_VIDEO ) < 0 ) {
@@ -38,11 +55,11 @@ int init(SDL_Window **window, SDL_Renderer **renderer) {
 
   SDL_SetRenderDrawColor(*renderer, 0xFF, 0xFF, 0xFF, 0xFF);
 
-  int imgFlags = IMG_INIT_PNG;
-  if( !( IMG_Init( imgFlags ) & imgFlags ) ) {
-    printf( "SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError() );
-    return 1;
-  }
+  /* int imgFlags = IMG_INIT_PNG; */
+  /* if( !( IMG_Init( imgFlags ) & imgFlags ) ) { */
+  /*   printf( "SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError() ); */
+  /*   return 1; */
+  /* } */
 
   return 0;
 }
@@ -140,20 +157,83 @@ void move(SDL_Rect* player, int velocity) {
   player->y = y;
 }
 
-int main(int argc, char *args[]){
-  static int quit = FALSE;
-  static int show_title = TRUE;
-
-  static SDL_Window *window = NULL;
-  static SDL_Surface *screen_surface = NULL;
-  static SDL_Surface *png_surface = NULL;
-  static SDL_Texture *texture = NULL;
-  static SDL_Texture *ball_texture = NULL;
-  static SDL_Renderer *renderer = NULL;
+void loop() {
   static SDL_Event event;
 
-  SDL_Rect player1, player2;
-  int player1_vel = 0, player2_vel = 0;
+  while(SDL_PollEvent(&event) != 0) {
+    if (event.type == SDL_QUIT){
+      quit = TRUE;
+    }
+    if( event.type == SDL_KEYDOWN && event.key.repeat == 0 ) {
+      switch( event.key.keysym.sym ) {
+      case SDLK_UP: player1_vel -= velocity; break;
+      case SDLK_DOWN: player1_vel += velocity; break;
+      case SDLK_w: player2_vel -= velocity; break;
+      case SDLK_s: player2_vel += velocity; break;
+      case SDLK_q : quit = TRUE; break;
+      default: break;
+      }
+    } else if( event.type == SDL_KEYUP && event.key.repeat == 0 ) {
+      switch( event.key.keysym.sym ) {
+      case SDLK_UP: player1_vel += velocity; break;
+      case SDLK_DOWN: player1_vel -= velocity; break;
+      case SDLK_w: player2_vel += velocity; break;
+      case SDLK_s: player2_vel -= velocity; break;
+      default: break;
+      }
+    }
+  }
+
+  move(&player1, player1_vel);
+  move(&player2, player2_vel);
+
+  //Render texture to screen
+  if (show_title) {
+    SDL_RenderClear(renderer);
+    SDL_RenderCopy(renderer, texture, NULL, NULL );
+    SDL_RenderPresent(renderer);
+
+    show_title = FALSE;
+    SDL_Delay(500);
+    return;
+  }
+
+  SDL_RenderClear(renderer);
+
+  SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+  SDL_RenderClear(renderer);
+
+  //Render player1
+  SDL_SetRenderDrawColor( renderer, 0xFF, 0x00, 0x00, 0xFF );
+  SDL_RenderFillRect( renderer, &player1 );
+
+  //Render player2
+  SDL_SetRenderDrawColor( renderer, 0x00, 0x00, 0xFF, 0xFF );
+  SDL_RenderFillRect( renderer, &player2 );
+
+  // Render ball
+  SDL_Rect renderQuad = {SCREEN_WIDTH/2 - ball_w/2 + player1_vel,
+                         SCREEN_HEIGHT/2-ball_h/2 + player2_vel,
+                         ball_w,
+                         ball_h};
+  SDL_Rect *clip = NULL;
+  SDL_Point *center = NULL;
+  double angle = 0.0;
+  SDL_RendererFlip flip = SDL_FLIP_NONE;
+
+  SDL_RenderCopyEx(renderer,
+                   ball_texture,
+                   clip,
+                   &renderQuad,
+                   angle,
+                   center,
+                   SDL_FLIP_NONE);
+
+  //Update screen
+  SDL_RenderPresent(renderer);
+}
+    
+int main(int argc, char *args[]){
 
   if (init(&window, &renderer) == 1){
     printf("Failed to init system\n");
@@ -187,80 +267,8 @@ int main(int argc, char *args[]){
                   10,
                   SCREEN_HEIGHT / 6);
 
-  while(!quit){
-    while(SDL_PollEvent(&event) != 0) {
-      if (event.type == SDL_QUIT){
-        quit = TRUE;
-      }
-      if( event.type == SDL_KEYDOWN && event.key.repeat == 0 ) {
-        switch( event.key.keysym.sym ) {
-        case SDLK_UP: player1_vel -= velocity; break;
-        case SDLK_DOWN: player1_vel += velocity; break;
-        case SDLK_w: player2_vel -= velocity; break;
-        case SDLK_s: player2_vel += velocity; break;
-        case SDLK_q : quit = TRUE; break;
-        default: break;
-        }
-      } else if( event.type == SDL_KEYUP && event.key.repeat == 0 ) {
-        switch( event.key.keysym.sym ) {
-        case SDLK_UP: player1_vel += velocity; break;
-        case SDLK_DOWN: player1_vel -= velocity; break;
-        case SDLK_w: player2_vel += velocity; break;
-        case SDLK_s: player2_vel -= velocity; break;
-        default: break;
-        }
-      }
-    }
-
-    move(&player1, player1_vel);
-    move(&player2, player2_vel);
-
-    //Render texture to screen
-    if (show_title) {
-      SDL_RenderClear(renderer);
-      SDL_RenderCopy(renderer, texture, NULL, NULL );
-      SDL_RenderPresent(renderer);
-
-      show_title = FALSE;
-      SDL_Delay(500);
-      continue;
-    }
-
-    SDL_RenderClear(renderer);
-
-    SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
-    SDL_RenderClear(renderer);
-
-    //Render player1
-    SDL_SetRenderDrawColor( renderer, 0xFF, 0x00, 0x00, 0xFF );
-    SDL_RenderFillRect( renderer, &player1 );
-
-    //Render player2
-    SDL_SetRenderDrawColor( renderer, 0x00, 0x00, 0xFF, 0xFF );
-    SDL_RenderFillRect( renderer, &player2 );
-
-    // Render ball
-    SDL_Rect renderQuad = {SCREEN_WIDTH/2 - ball_w/2,
-                           SCREEN_HEIGHT/2-ball_h/2,
-                           ball_w,
-                           ball_h};
-    SDL_Rect *clip = NULL;
-    SDL_Point *center = NULL;
-    double angle = 0.0;
-    SDL_RendererFlip flip = SDL_FLIP_NONE;
-
-    SDL_RenderCopyEx(renderer,
-                     ball_texture,
-                     clip,
-                     &renderQuad,
-                     angle,
-                     center,
-                     SDL_FLIP_NONE);
-
-    //Update screen
-    SDL_RenderPresent(renderer);
-  }
-
+  emscripten_set_main_loop(loop, 0, 1);
+  
   SDL_DestroyTexture(texture);
   texture = NULL;
 
